a9c4974ffaa958d4de49687554cff50cae84f146
Mart0GD <marty0545@gmail.com>
2025-03-12 21:17:03 +0200

    Seminar 02 - Streams

--- Seminars/Group One/02. Streams/Cpp files/Task1.cpp
+++ Seminars/Group One/02. Streams/Cpp files/Task1.cpp
@@ -20,26 +20,33 @@
 параметър.
 */
 
-void ClearWhitespace();
-int PrintFileLength(std::ifstream& file);
-int PrintNWSCCount(std::ifstream& file);
+int getFileLength(std::ifstream& file);
+int getNWSCharactersCount(std::ifstream& file);
 void PrintWordsCount(std::ifstream& file);
 void PrintRowsCount(std::ifstream& file);
 void PrintNonEmptyRowsCount(std::ifstream& file);
 void PrintStatistic(std::ifstream& file);
 void PrintHelpMenu();
+void revert(std::istream& stream);
 
 int main() {
 
 	char command;
 	char* fileName = new(std::nothrow) char[MAX_FILE_NAME];
-	if (!fileName) return 1;
+	if (!fileName) return -1;
 
-	std::cin.ignore(1, '-');
-	std::cin >> command;
-	ClearWhitespace();
+	std::cout << "Enter command --> ";
+
+	char ch;
+	std::cin >> ch >> command;
+	if (!std::cin || ch != '-') {
+		std::cerr << "Invalid input!";
+		return -1;
+	}
+
 	std::ifstream file;
-
+	std::cout << "Enter file name --> ";
+	std::cin.ignore();
 	do
 	{
 		std::cin.getline(fileName, MAX_FILE_NAME);
@@ -56,12 +63,12 @@ int main() {
 		switch (command)
 		{
 			case 'c':
-				PrintFileLength(file);
+				std::cout << "File length --> " << getFileLength(file) << std::endl;
 				done = true;
 			break;
 
 			case 'C':
-				PrintNWSCCount(file);
+				std::cout << "NWS Characters --> " << getNWSCharactersCount(file) << std::endl;
 				done = true;
 			break;
 
@@ -89,15 +96,15 @@ int main() {
 				PrintHelpMenu();
 				done = false;
 			break;
-
-
 			
 		}
 
 		if (!done) {
-			ClearWhitespace();
-			std::cin.ignore(1, '-');
-			std::cin >> command;
+			command = '\0';
+			do
+			{
+				std::cin >> ch >> command;
+			} while (ch != '-' && !command);
 		}
 	} while (!done);
 
@@ -106,53 +113,41 @@ int main() {
 	return 0;
 }
 
-
-void ClearWhitespace() {
-	while (std::cin.peek() == ' ' || std::cin.peek() == '\n')
-		std::cin.ignore();
-}
-
 bool IsWhiteSpace(char chr) {
 	return !(chr > 32 && chr < 127);
 }
 
-int PrintFileLength(std::ifstream& file) {
+int getFileLength(std::ifstream& file) {
 	file.clear();
 	file.seekg(0, std::ios::end);
 
-	std::cout << "File length --> " << file.tellg() << std::endl;
 	return file.tellg();
 }
 
-int PrintNWSCCount(std::ifstream& file) {
-	file.clear();
-	file.seekg(0, std::ios::beg);
+int getNWSCharactersCount(std::ifstream& file) {
+	revert(file);
 
 	int cnt = 0;
-	while (file.peek() != EOF){
+	while (!file.eof())
 		if (!IsWhiteSpace(file.get())) cnt++;
-	}
 
-	std::cout << "NWS Characters --> " << cnt << std::endl;
 	return cnt;
 }
 
 void PrintWordsCount(std::ifstream& file) {
-	file.clear();
-	file.seekg(0, std::ios::beg);
+	revert(file);
 
 	int cnt = 0;
-
 	bool wordStarted = false;
 	char c = ' ';
-	while (file.peek() != EOF){
+	while (!file.eof()){
 		c = file.get();
 
-		if (IsWhiteSpace(c) && wordStarted){
+		if (wordStarted && IsWhiteSpace(c)){
 			wordStarted = false;
 			cnt++;
 		}
-		else if (!IsWhiteSpace(c) && !wordStarted) {
+		else if (!wordStarted && !IsWhiteSpace(c)) {
 			wordStarted = true;
 		}
 	}
@@ -162,22 +157,21 @@ void PrintWordsCount(std::ifstream& file) {
 }
 
 void PrintRowsCount(std::ifstream& file) {
-	file.clear();
-	file.seekg(0, std::ios::beg);
+	revert(file);
 
-	int rows = 0; int charactersCnt = 0;
+	int rows = 0;
+	char ch = '\0';
 	while (!file.eof()){
-		if (file.get() == '\n') rows++;
-		charactersCnt++;
+		file.get(ch);
+		if (ch == '\n') rows++;
 	}
-	if(charactersCnt > 0) rows++;
+	if(ch) rows++;
 
 	std::cout << "Rows count " << rows << std::endl;;
 }
 
 void PrintNonEmptyRowsCount(std::ifstream& file) {
-	file.clear(); 
-	file.seekg(0, std::ios::beg);
+	revert(file);
 
 	int rows = 0;
 	int characters = 0;
@@ -197,9 +191,9 @@ void PrintNonEmptyRowsCount(std::ifstream& file) {
 }
 
 void PrintStatistic(std::ifstream& file) {
-	double allCharacters = PrintFileLength(file);
+	double allCharacters = getFileLength(file);
 
-	double nws = PrintNWSCCount(file);
+	double nws = getNWSCharactersCount(file);
 
 	PrintWordsCount(file);
 
@@ -221,4 +215,8 @@ void PrintHelpMenu() {
 		<< "-h --> Help!\n";
 }
 
+void revert(std::istream& stream) {
+	stream.clear();
+	stream.seekg(0, std::ios::beg);
+}
 
